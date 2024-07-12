16b57a1fa859e377d2177ddd08f56e8ed76007b2
mikolajchm <chmielewski.5454@gmail.com>
2024-07-12 23:35:40 +0200

    task 22.3 in progress

---
+++ models/product.model.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  client: { type: String, required: true },
+});
+
+module.exports = mongoose.model('Product', productSchema);
+
+
+
+
